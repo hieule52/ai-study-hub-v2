7070f04ff042bbf3d6ed5fbd3a6632a0073f4cf5
Le Dien Hieu <lehieu2026.in@gmail.com>
2026-06-02 12:26:57 +0700

    update project 2-6

--- database/upgrade_v4.sql
+++
@@ -1,8 +0,0 @@
--- Upgrade script to support video/text progress tracking, quiz passing score, and course completion status
-
-ALTER TABLE lesson_progress ADD COLUMN video_progress INT NOT NULL DEFAULT 0;
-ALTER TABLE lesson_progress ADD COLUMN text_progress INT NOT NULL DEFAULT 0;
-
-ALTER TABLE quizzes ADD COLUMN passing_score INT NOT NULL DEFAULT 80;
-
-ALTER TABLE enrollments ADD COLUMN course_status VARCHAR(50) NOT NULL DEFAULT 'learning';
